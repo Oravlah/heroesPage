1385824734be064dd17b8514fbdf94050fcc4d6c
Oravlah <oravla.hhh@gmail.com>
2025-03-04 17:14:50 -0300

    Se implementa tailwind

--- src/app/layout/layout/layout.component.ts
+++ src/app/layout/layout/layout.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import HeaderComponent from '../../components/header/header.component';
 import BodyComponent from '../../components/body/body.component';
 import { RouterModule } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
 
 
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [HeaderComponent, BodyComponent, RouterModule],
+  imports: [HeaderComponent, BodyComponent, RouterModule, MatSidenavModule],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
